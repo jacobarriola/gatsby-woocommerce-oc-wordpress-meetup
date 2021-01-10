d2a7132eb29a3ed8577992d3c797413368aca7df
Jacob Arriola <jacob.arriola@gmail.com>
2021-01-09 17:45:03 -0800

    Build errors - ensure React is define in JSX land

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { ApolloProvider } from '@apollo/client'
 import { client } from './src/components/apolloClient'
 import { AppProvider } from './src/components/context'
